143375fa36063df91c0db15460a8b76a585d25ee
ac88 <mmaxpokerr@gmail.com>
2021-04-30 19:29:58 +0300

    scales-project-nextjs

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Головна',
+  },
+  {
+    path: 'feature',
+    label: 'Переваги',
+  },
+  {
+    path: 'pricing',
+    label: 'Ціна',
+  },
+  {
+    path: 'testimonial',
+    label: 'Відгуки',
+  },
+];
