9246257e7467c55f52c8b3e38037ac603493bd98
ac88 <mmaxpokerr@gmail.com>
2021-05-17 19:17:38 +0300

    scales-project-nextjs-version-0.7.

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,6 @@ export default function IndexPage() {
           <Reminder />
           <WorkFlow />
           <Price />
-          {/*<TeamSection />*/}
-          <TestimonialCard />
         </Layout>
     </ThemeProvider>
   );
