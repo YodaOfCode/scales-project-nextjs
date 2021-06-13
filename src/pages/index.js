3c862acd88592b21e82264e965fc15fbec81ac08
YodaOfCode <mmaxpokerr@gmail.com>
2021-06-13 18:40:23 +0300

    Try to add Google Analytics version - 0.1.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,10 @@ import WorkFlow from '../sections/workflow/workflow';
 import Price from '../sections/price/price';
 import TestimonialCard from '../sections/testimonials/testimonial';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-CNNMFBRGZ2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
