a5adf976ebe16764dd82fbfcd2f3670e9eeb5148
YodaOfCode <mmaxpokerr@gmail.com>
2021-06-16 12:53:04 +0300

    Try to add Google Analytics version - 0.6.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,8 +11,22 @@ class CustomDocument extends Document {
         return (
             <Html lang="uk-UK">
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CNNMFBRGZ2"/>
-                    <script src="/ga.js" async/>
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
+                    />
                 </Head>
                 <body>
                 <Main/>
